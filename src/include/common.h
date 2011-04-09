ac41a5193462b9831969f2da4334d8c63b929b43
Emil Lauridsen <mine809@gmail.com>
2011-04-09 14:58:46 +0200

    Add 64-bit data types (long long)

--- src/include/common.h
+++ src/include/common.h
@@ -7,12 +7,14 @@
 // Some standard typedefs, to standardise sizes across platforms.
 // These typedefs are written for 32-bit X86.
 typedef unsigned int size_t;
-typedef unsigned int   uint32_t;
-typedef          int   int32_t;
-typedef unsigned short uint16_t;
-typedef          short int16_t;
-typedef unsigned char  uint8_t;
-typedef          char  int8_t;
+typedef unsigned long long uint64_t
+typedef			 long long int64_t
+typedef unsigned int   	   uint32_t;
+typedef          int   	   int32_t;
+typedef unsigned short     uint16_t;
+typedef          short     int16_t;
+typedef unsigned char      uint8_t;
+typedef          char      int8_t;
 
 #include "printk.h"
 #include "panic.h"
